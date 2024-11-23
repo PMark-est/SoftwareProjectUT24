852d0c55c39362c34deb69b18dfac653bab20464
tonyNeStark <anton3360@gmail.com>
2024-11-23 17:11:41 +0200

    Tests fixed

--- frontend/cypress/e2e/highlight_test.cy.ts
+++ frontend/cypress/e2e/highlight_test.cy.ts
@@ -11,12 +11,12 @@ describe('Search test', () => {
     // Click the search button
     cy.get('#sendBtn').click();
 
-    cy.get('#left-column > div > div > div.tab-pane.ng-scope.active > div > kwic > div > div.table_scrollarea > table > tbody > tr:nth-child(2) > td.left.ng-scope > kwic-word:nth-child(6) > span')
+    cy.get('#sentence_root > span > span:nth-child(2)')
       .should('be.visible') // Ensure the element is visible
       .should('have.css', 'background-color', 'rgb(250, 145, 137)'); // Check for red background
 
-    cy.get('#left-column > div > div > div.tab-pane.ng-scope.active > div > kwic > div > div.table_scrollarea > table > tbody > tr:nth-child(14) > td.left.ng-scope > kwic-word:nth-child(7) > span')
+    cy.get('#left-column > div > div > div.tab-pane.ng-scope.active > div > kwic > div > div.table_scrollarea > table > tbody > tr:nth-child(9) > td.left.ng-scope > kwic-word:nth-child(2) > span > span')
       .should('be.visible') // Ensure the element is visible
-      .should('have.css', 'background-color', 'rgb(70, 224, 121)'); // Check for red background
+      .should('have.css', 'background-color', 'rgb(209, 189, 255)'); 
   });
 });
--- frontend/cypress/e2e/popupTest.cy.ts
+++ frontend/cypress/e2e/popupTest.cy.ts
@@ -12,12 +12,12 @@ describe('Search test', () => {
     cy.get('#sendBtn').click();
 
      // Click the specified element to trigger the popup
-     cy.get('#left-column > div > div > div.tab-pane.ng-scope.active > div > kwic > div > div.table_scrollarea > table > tbody > tr:nth-child(9) > td.left.ng-scope > kwic-word:nth-child(11)')
+     cy.get('#left-column > div > div > div.tab-pane.ng-scope.active > div > kwic > div > div.table_scrollarea > table > tbody > tr:nth-child(12) > td.left.ng-scope > kwic-word:nth-child(4)')
      .should('be.visible')
      .click();
 
    // Assert that the popup now appears
-    cy.get('#left-column > div > div > div.tab-pane.ng-scope.active > div > kwic > div > div.table_scrollarea > div')
+    cy.get('#sentence_root > span > span')
      .should('be.visible');
 
 
